2511b929c2f4a086bb5ec686fa947d622b4ac9a3
Pavel Shiltsov <pd.shiltsov@gmail.com>
2021-06-15 19:55:04 +0300

    15.06

--- urljoin-study.py
+++ urljoin-study.py
@@ -1,3 +1,16 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse, urlsplit
+import os
 
-print(urljoin('https://medestetik.ru/1/', 'index.php'))
+# print(urljoin('https://medestetik.ru/1/', 'index.php'))
+url = "https://www.medestetik.ru:8000/faq/topic.php;12?page=10&a=23"
+
+print(urlparse(url).fragment == '')
+print(urlsplit(url))
+print(urlparse(url, allow_fragments=False).geturl())
+
+#u1 = 'http://www.mgido.ru/abc/'
+#u2 = '/ino/smi/1.php'
+
+#print(urljoin(u1, u2))
+
+print(os.path.join('data','index.php'))
